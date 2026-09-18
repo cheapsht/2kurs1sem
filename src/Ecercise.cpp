fe7931d364e99f262f4046e85d919bca54e93e59
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-09-18 16:45:52 +0300

    Update Ecercise.cpp

--- src/Ecercise.cpp
+++ src/Ecercise.cpp
@@ -1,13 +1,9 @@
 #include "Exercise.hpp"
 
 Exercise::Exercise(const Matrix<int>& matrix, Generator* generator)
-    : matrix(matrix), generator(generator)
-{
-}
+    : matrix(matrix), generator(generator) {}
 
-Exercise::~Exercise()
-{
-}
+Exercise::~Exercise() {}
 
 Matrix<int> Exercise::getMatrix() const
 {
