a0526c3a3812045b311083f5bce204a3dfdaf5ff
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-09-19 12:00:58 +0300

    Create IStreamGenerator.cpp

---
+++ src/IStreamGenerator.cpp
@@ -0,0 +1,10 @@
+#include "IStreamGenerator.hpp"
+
+IStreamGenerator::IStreamGenerator(std::istream& in) : in(in) {}
+
+int IStreamGenerator::generate()
+{
+    int value = 0;
+    this->in >> value;
+    return value;
+}
